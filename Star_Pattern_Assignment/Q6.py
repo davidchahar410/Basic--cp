2d3bc4aa4c58afc847fc3bb5c9660fc43f1ced0d
davidchahar410 <davidchahar410@gmail.com>
2025-09-08 20:25:23 +0530

    Add files via upload

---
+++ Star_Pattern_Assignment/Q6.py
@@ -0,0 +1,14 @@
+# Ques 6: 
+# n = 5 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+
+n=5
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print("*",end=" ")
+    print()
---
+++ Star_Pattern_Assignment/Q8.py
@@ -0,0 +1,14 @@
+# Ques 8: 
+# n = 5 
+# * * 
+# * * 
+# * 
+# * * 
+# * *
+
+n=5
+for i in range(n):
+    if i == 2:
+        print("* "*1)
+    else:
+        print("* "*2)
